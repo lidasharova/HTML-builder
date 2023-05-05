6c2a995475e4e30cb00b4441b9c362340d705831
lidasharova <Lida050496@yandex.ru>
2023-05-05 20:28:24 +0700

    feat: done 4th task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,18 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('node:readline/promises');
 
-//путь к файлу
 const pathFile = path.join(__dirname, 'new.txt');
-//поток на запись/ создание файла
 const WS = fs.createWriteStream(pathFile, 'utf-8');
-//поток на чтение
-// const RS = fs.createReadStream(pathFile);
-//создание интерфейса для чтения данных из потока Readable
 const RL = readline.createInterface({ input, output });
 
-//предлагаем записать текст
 RL.write('Привет! Как твои дела? :)\n');
-
 RL.on('line', (answer) => {
   if (answer !== 'exit') {
     WS.write(`${answer}\n`);
@@ -23,19 +16,16 @@ RL.on('line', (answer) => {
   }
 });
 
-//событие на клавиши Ctrl+C
 RL.on('SIGINT', () => {
   RL.close();
 });
 
-//событие на ввод exit
 RL.on('data', (data) => {
   if (data.toString().trim() === 'exit') {
     RL.close();
   }
 });
 
-//прощальное сообщение
 process.on('exit', () => {
   console.log('\nВсе будет отлично! Ты только не опускай руки!))');
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 
+//создаем новую папку
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+//очищаем папку
+fs.readdir(
+  path.join(__dirname, 'files-copy'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+    else {
+      files.forEach((file) => {
+        fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    }
+  }
+);
+
+//копируем файлы
+fs.readdir(
+  path.join(__dirname, 'files'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+    else {
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(__dirname, 'files', file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      });
+    }
+  }
+);
