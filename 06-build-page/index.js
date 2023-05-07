f1942ee9e069275f176969cb8ddf47f65e0af376
lidasharova <Lida050496@yandex.ru>
2023-05-07 15:53:09 +0700

    6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,103 +2,99 @@ const fs = require('fs');
 const path = require('path');
 const pathDir = path.join(__dirname, 'styles');
 
-let template;
+let template = '';
 
 //создаем новую папку project-dist
 fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
   if (err) throw err;
+  else {
+    fs.mkdir(
+      path.join(__dirname, 'project-dist', 'assets'),
+      { recursive: true },
+      (err) => {
+        if (err) {
+          throw err;
+        } else {
+          //очищаем папку project-dist если в ней что-то есть
+          fs.readdir(
+            path.join(__dirname, 'project-dist'),
+            { withFileTypes: true },
+            (err, files) => {
+              if (err) throw err;
+              else {
+                files.forEach((file) => {
+                  if (file.isFile()) {
+                    fs.unlink(
+                      path.join(__dirname, 'project-dist', file.name),
+                      (err) => {
+                        if (err) throw err;
+                      }
+                    );
+                  } else {
+                    fs.rmdir(
+                      path.join(__dirname, 'project-dist', file.name),
+                      { recursive: true },
+                      (err) => {
+                        if (err) throw err;
+                      }
+                    );
+                  }
+                });
+              }
+            }
+          );
+          fs.readdir(
+            path.join(__dirname, 'assets'),
+            { withFileTypes: true },
+            (err, files) => {
+              if (err) throw err;
+              files.forEach((file) => {
+                if (file.isFile()) {
+                  fs.copyFile(
+                    path.join(__dirname, 'assets', file.name),
+                    path.join(__dirname, 'project-dist', 'assets', file.name),
+                    (err) => {
+                      if (err) throw err;
+                    }
+                  );
+                } else {
+                  copyMkdir(
+                    path.join(__dirname, 'assets', file.name),
+                    path.join(__dirname, 'project-dist', 'assets', file.name)
+                  );
+                }
+              });
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
-//создаем новую папку assets
-fs.mkdir(
-  path.join(__dirname, 'project-dist', 'assets'),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-//создаем новые папки внутри assets
-fs.mkdir(
-  path.join(__dirname, 'project-dist', 'assets', 'fonts'),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-fs.mkdir(
-  path.join(__dirname, 'project-dist', 'assets', 'img'),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-fs.mkdir(
-  path.join(__dirname, 'project-dist', 'assets', 'svg'),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-
-//копируем папку 'fonts'
-fs.readdir(
-  path.join(__dirname, 'assets', 'fonts'),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) throw err;
-    else {
+//ф-ция для копирования папок
+function copyMkdir(fromDir, inDir) {
+  fs.mkdir(inDir, { recursive: true }, () => {
+    fs.readdir(fromDir, { withFileTypes: true }, (err, files) => {
       files.forEach((file) => {
-        fs.copyFile(
-          path.join(__dirname, 'assets', 'fonts', file.name),
-          path.join(__dirname, 'project-dist', 'assets', 'fonts', file.name),
-          (err) => {
-            if (err) throw err;
-          }
-        );
+        if (file.isFile()) {
+          fs.copyFile(
+            path.join(fromDir, file.name),
+            path.join(inDir, file.name),
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        } else {
+          copyMkdir(
+            path.join(fromDir, file.name),
+            path.join(fromDir, file.name)
+          );
+        }
       });
-    }
-  }
-);
-
-//копируем папку 'img'
-fs.readdir(
-  path.join(__dirname, 'assets', 'img'),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) throw err;
-    else {
-      files.forEach((file) => {
-        fs.copyFile(
-          path.join(__dirname, 'assets', 'img', file.name),
-          path.join(__dirname, 'project-dist', 'assets', 'img', file.name),
-          (err) => {
-            if (err) throw err;
-          }
-        );
-      });
-    }
-  }
-);
-
-//копируем папку 'svg'
-fs.readdir(
-  path.join(__dirname, 'assets', 'svg'),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) throw err;
-    else {
-      files.forEach((file) => {
-        fs.copyFile(
-          path.join(__dirname, 'assets', 'svg', file.name),
-          path.join(__dirname, 'project-dist', 'assets', 'svg', file.name),
-          (err) => {
-            if (err) throw err;
-          }
-        );
-      });
-    }
-  }
-);
+    });
+  });
+}
 
 //чтение и запись файла template.html в переменную;
 const readableStream = fs.createReadStream(
@@ -107,21 +103,36 @@ const readableStream = fs.createReadStream(
 );
 readableStream.on('data', (data) => {
   template = data;
-  // console.log(template);
 });
 
-//создаем и запись в файл index.html
-const WRSHTML = fs.createWriteStream(
-  path.join(__dirname, 'project-dist', 'index.html'),
-  'utf-8'
+//заменa тегов на компоненты
+fs.readdir(
+  path.join(__dirname, 'components'),
+  { withFileTypes: true },
+  (err, files) => {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        let ext = file.name.toString().split('.')[1];
+        if (ext === 'html') {
+          let tag = file.name.split('.')[0];
+          let name = file.name;
+          fs.readFile(
+            path.join(__dirname, 'components', name),
+            'utf-8',
+            (err, files) => {
+              template = template.replace(`{{${tag}}}`, `${files}`);
+              const WSHTML = fs.createWriteStream(
+                path.join(__dirname, 'project-dist', 'index.html')
+              );
+              WSHTML.write(template);
+              WSHTML.close();
+            }
+          );
+        }
+      }
+    });
+  }
 );
-const RSHTML = fs.createReadStream(
-  path.join(__dirname, 'project-dist', 'index.html'),
-  'utf-8'
-);
-RSHTML.on('data', (template) => {
-  WRSHTML.write(template);
-});
 
 //файл стилей
 const pathNewFile = path.join(__dirname, 'project-dist', 'style.css');
